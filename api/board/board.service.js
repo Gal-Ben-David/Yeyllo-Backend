0be217b273cd4961bff54cd8fd3d5625eddbc006
OferKoren <oferkoren2@gmail.com>
2024-11-19 16:48:22 +0200

    Fix bus with closed board

--- api/board/board.service.js
+++ api/board/board.service.js
@@ -6,127 +6,126 @@ import { authService } from '../auth/auth.service.js'
 import { utilService } from '../../services/util.service.js'
 
 export const boardService = {
-	remove,
-	query,
-	getById,
-	add,
-	update,
-	findByIdAndUpdate
-	// addToyMsg,
-	// removeToyMsg,
+    remove,
+    query,
+    getById,
+    add,
+    update,
+    findByIdAndUpdate,
+    // addToyMsg,
+    // removeToyMsg,
 }
 
 async function query(filterBy = {}) {
-	try {
-		// const criteria = {
-		// 	name: { $regex: filterBy.name, $options: 'i' },
-		// }
-		// if (filterBy.labels && filterBy.labels.length > 0) {
-		// 	criteria.labels = { $in: filterBy.labels }
-		// }
-		// if (filterBy.price !== 0) {
-		// 	criteria.price = { $lte: filterBy.price }
-		// }
-		// if (filterBy.inStock !== 'all') {
-		// 	criteria.inStock = (filterBy.inStock === 'available')
-		// }
+    try {
+        // const criteria = {
+        // 	name: { $regex: filterBy.name, $options: 'i' },
+        // }
+        // if (filterBy.labels && filterBy.labels.length > 0) {
+        // 	criteria.labels = { $in: filterBy.labels }
+        // }
+        // if (filterBy.price !== 0) {
+        // 	criteria.price = { $lte: filterBy.price }
+        // }
+        // if (filterBy.inStock !== 'all') {
+        // 	criteria.inStock = (filterBy.inStock === 'available')
+        // }
 
-		// let sortOption = {}
-		// if (filterBy.selector === 'name') {
-		// 	sortOption = { name: 1 }
-		// } else if (filterBy.selector === 'price') {
-		// 	sortOption = { price: 1 }
-		// } else if (filterBy.selector === 'createdAt') {
-		// 	sortOption = { createdAt: 1 }
-		// }
-		const collection = await dbService.getCollection('board')
-		// var toys = await collection.find(criteria).sort(sortOption).toArray()
-		var boards = await collection.find().toArray()
-		return boards
-	} catch (err) {
-		loggerService.error('cannot find boards', err)
-		throw err
-	}
+        // let sortOption = {}
+        // if (filterBy.selector === 'name') {
+        // 	sortOption = { name: 1 }
+        // } else if (filterBy.selector === 'price') {
+        // 	sortOption = { price: 1 }
+        // } else if (filterBy.selector === 'createdAt') {
+        // 	sortOption = { createdAt: 1 }
+        // }
+        const collection = await dbService.getCollection('board')
+        // var toys = await collection.find(criteria).sort(sortOption).toArray()
+        var boards = await collection.find().toArray()
+        return boards
+    } catch (err) {
+        loggerService.error('cannot find boards', err)
+        throw err
+    }
 }
 
 async function getById(boardId) {
-	try {
-		const collection = await dbService.getCollection('board')
-		const board = await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
-		board.createdAt = board._id.getTimestamp()
-		return board
-	} catch (err) {
-		loggerService.error(`while finding board ${boardId}`, err)
-		throw err
-	}
+    try {
+        const collection = await dbService.getCollection('board')
+        const board = await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+        board.createdAt = board._id.getTimestamp()
+        return board
+    } catch (err) {
+        loggerService.error(`while finding board ${boardId}`, err)
+        throw err
+    }
 }
 
 async function remove(boardId) {
-	try {
-		const collection = await dbService.getCollection('board')
-		const { deletedCount } = await collection.deleteOne({ _id: ObjectId.createFromHexString(boardId) })
-		return deletedCount
-	} catch (err) {
-		loggerService.error(`cannot remove board ${boardId}`, err)
-		throw err
-	}
+    try {
+        const collection = await dbService.getCollection('board')
+        const { deletedCount } = await collection.deleteOne({ _id: ObjectId.createFromHexString(boardId) })
+        return deletedCount
+    } catch (err) {
+        loggerService.error(`cannot remove board ${boardId}`, err)
+        throw err
+    }
 }
 
 async function add(board) {
-	try {
-		const collection = await dbService.getCollection('board')
-		await collection.insertOne(board)
-		return board
-	} catch (err) {
-		loggerService.error('cannot insert board', err)
-		throw err
-	}
+    try {
+        const collection = await dbService.getCollection('board')
+        await collection.insertOne(board)
+        return board
+    } catch (err) {
+        loggerService.error('cannot insert board', err)
+        throw err
+    }
 }
 
 async function update(board) {
-	try {
-		const updatedBoard = {
-			title: board.title,
-			isStarred: board.isStarred,
-			style: board.style,
-			labels: board.labels,
-			members: board.members,
-			groups: board.groups,
-			urls: board.urls
-		}
+    try {
+        const updatedBoard = {
+            title: board.title,
+            isStarred: board.isStarred,
+            style: board.style,
+            labels: board.labels,
+            members: board.members,
+            groups: board.groups,
+            urls: board.urls,
+            isClosed: board.isClosed,
+        }
 
-		const criteria = { _id: ObjectId.createFromHexString(board._id) }
-		const collection = await dbService.getCollection('board')
-		await collection.updateOne(criteria, { $set: updatedBoard })
+        const criteria = { _id: ObjectId.createFromHexString(board._id) }
+        const collection = await dbService.getCollection('board')
+        await collection.updateOne(criteria, { $set: updatedBoard })
 
-		const boardAfter = await collection.findOne({ _id: ObjectId.createFromHexString(board._id) })
+        const boardAfter = await collection.findOne({ _id: ObjectId.createFromHexString(board._id) })
 
-		return boardAfter
-	} catch (err) {
-		loggerService.error(`cannot update board ${board._id}`, err)
-		throw err
-	}
+        return boardAfter
+    } catch (err) {
+        loggerService.error(`cannot update board ${board._id}`, err)
+        throw err
+    }
 }
 
 async function findByIdAndUpdate(boardId, activity) {
-	try {
+    try {
+        const collection = await dbService.getCollection('board')
 
-		const collection = await dbService.getCollection('board')
-
-		const criteria = { _id: ObjectId.createFromHexString(boardId) }
-		await collection.updateOne(criteria, {
-			$push: {
-				activities: {
-					$each: [activity],
-					$position: 0
-				}
-			}
-		})
-
-	} catch (err) {
-		console.error('Failed to update board', err)
-		throw err
-	}
+        const criteria = { _id: ObjectId.createFromHexString(boardId) }
+        await collection.updateOne(criteria, {
+            $push: {
+                activities: {
+                    $each: [activity],
+                    $position: 0,
+                },
+            },
+        })
+    } catch (err) {
+        console.error('Failed to update board', err)
+        throw err
+    }
 }
 
 // async function addBoardMsg(toyId, msg) {
